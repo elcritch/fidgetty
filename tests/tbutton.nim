cfe6902bb8b7de665357fe948a271742a13a31a8
Jaremy Creechley <creechley@gmail.com>
2022-04-20 02:01:57 -0700

    tests

--- tests/tbutton.nim
+++ tests/tbutton.nim
@@ -6,8 +6,11 @@ loadFont("IBM Plex Sans", "IBMPlexSans-Regular.ttf")
 
 proc drawMain() =
   frame "main":
+    font "IBM Plex Sans", 16, 200, 0, hCenter, vCenter
     Widget button:
       text: "click me!"
+      click:
+        echo "hi!"
 
 
 startFidget(drawMain, uiScale=2.0)
