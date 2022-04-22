df0abf50301d3bdce15f9e408e67a6a6189025e1
Jaremy Creechley <creechley@gmail.com>
2022-04-22 03:23:48 -0700

    dropdowns

--- src/fidgets/dropdown.nim
+++ src/fidgets/dropdown.nim
@@ -9,7 +9,6 @@ import fidgets
 
 loadFont("IBM Plex Sans", "IBMPlexSans-Regular.ttf")
 
-
 template dropUpY(n: Node, height: float32 = 0): bool =
   let a = n.descaled(screenBox)
   let b = root.descaled(screenBox)
@@ -139,45 +138,3 @@ proc dropdown*(
             box 0, 0, bw, 1.4*spad
           group "itempost":
             box 0, 0, bw, 12.5*spad
-
-  
-let dropItems = @["Nim", "UI", "in", "100%", "Nim", "to", 
-                  "OpenGL", "Immediate", "mode"]
-var dropIndexes = [-1, -1, -1]
-
-var dstate = Dropdown()
-
-proc drawMain() =
-  frame "main":
-    font "IBM Plex Sans", 16, 200, 0, hLeft, vBottom
-    box 1'em, 1'em, 100'pw - 1'em, 100'ph - 1'em
-    # offset 1'em, 1'em
-    # size 100.WPerc - 1'em, 100.HPerc - 1'em
-
-    Vertical:
-      # strokeLine 1.0, "#46D15F", 1.0
-      itemSpacing 1'em
-
-      text "first desc":
-        size 100'pw, 1'em
-        fill "#000d00"
-        characters "Dropdown example: "
-
-      dropdown(dropItems, dropIndexes[0], dstate)
-      dropdown(dropItems, dropIndexes[1], nil)
-      text "desc":
-        size 100'pw, 1'em
-        fill "#000d00"
-        characters "linked dropdowns: "
-      dropdown(dropItems, dropIndexes[2])
-      Widget dropdown:
-        items: dropItems
-        dropSelected: dropIndexes[2]
-        setup: box 0, 0, 12'em, 2'em
-      
-    # dropdown(dropItems, dropIndexes[2], nil) do:
-      # box 30, 80, 10.Em, 1.5.Em
-    # dropdown(dropItems, dropIndexes[2], nil) do:
-      # box 30, 120, 10.Em, 1.5.Em
-
-startFidget(drawMain, uiScale=2.0, h=600)
