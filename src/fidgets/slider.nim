756010124262ef32ac4f867fb1aca5ee52310971
Jaremy Creechley <creechley@gmail.com>
2022-04-26 05:56:00 -0700

    slider stuff

--- src/fidgets/slider.nim
+++ src/fidgets/slider.nim
@@ -2,7 +2,9 @@ import std/strformat
 
 import widgets
 
-proc slider*(value: var float) {.basicFidget.} =
+proc slider*(
+    value {.property: value.}: var float,
+): SliderState {.statefulFidget.} =
   ## Draw a progress bars 
 
   init:
@@ -10,26 +12,43 @@ proc slider*(value: var float) {.basicFidget.} =
     ## the default box size
     box 0, 0, 100.WPerc, 2.Em
 
+  properties:
+    pipDrag: bool
+  
   render:
+    let
+      # some basic calcs
+      sb = 0.3'em
+      bwOrig = current.box().w
+      bh = current.box().h
+      bw = current.box().w - bh/2 - 2*sb
 
-    box 260, 90, 250, 10
     onClick:
-      pipDrag = true
-    if pipDrag:
-      pipPos = int(mouse.descaled(pos).x - current.descaled(screenBox).x)
-      pipPos = clamp(pipPos, 1, 240)
-      pipDrag = buttonDown[MOUSE_LEFT]
+      self.pipDrag = true
+    if self.pipDrag:
+      value = (mouse.descaled(pos).x - current.descaled(screenBox).x)/bw
+      value = clamp(value, 0'f32, 1.0'f32)
+      self.pipDrag = buttonDown[MOUSE_LEFT]
+
+    let
+      wcalc = bw * float(value) + 0.001
+      pipPos = wcalc.clamp(0.0, bw)
+      wcalcBtn = (bw-sb) * float(value) + 0.001
+      pipPosBtn = wcalcBtn.clamp(0.0, bw)
+      # barH = bh - sb*sw
+
     rectangle "pip":
-      box pipPos, 0, 10, 10
+      # box pipPos-(bh/2-sb)/2+2*sb, sb, bh-2*sb, bh-2*sb
+      box pipPosBtn+sb, sb, bh-2*sb, bh-2*sb
       fill "#72bdd0"
-      cornerRadius 5
-    rectangle "fill":
-      box 0, 3, pipPos, 4
-      fill "#70bdcf"
       cornerRadius theme
       strokeLine theme
+    rectangle "fill":
+      box sb, sb, pipPos+bh/2, bh-2*sb
+      fill "#70bdcf"
+      cornerRadius theme
     rectangle "bg":
-      box 0, 3, 250, 4
+      box 0, 0, bwOrig, bh
       fill "#c2e3eb"
       cornerRadius theme
       strokeLine theme
