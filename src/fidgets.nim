527608a227bb8dd890458c54f7714fb6648724ba
Jaremy Creechley <creechley@gmail.com>
2022-04-30 14:51:50 -0700

    move theme

--- src/fidgets.nim
+++ src/fidgets.nim
@@ -3,3 +3,21 @@ import fidgets/widgets
 
 export fidget
 export widgets
+
+proc grayTheme*() =
+  setupWidgetTheme:
+    echo "GREY THEME"
+    let fs = 16'f32
+    theme.font("IBM Plex Sans", fs, 200, 0, hCenter, vCenter)
+    theme.corners(5)
+    theme.dropShadow(4, 0, 0, "#000000", 0.05)
+    theme.fill = parseHtml("#CDCDCD")
+    theme.textFill = parseHtml("#565555")
+    theme.foreground = parseHtml("#87E3FF", 0.77)
+    # theme.highlight = parseHtml("#77D3FF", 0.77)
+    theme.highlight = parseHtml("#87E3FF", 0.77)
+    theme.outerStroke = stroke(1, "#707070", 1.0)
+    theme.innerStroke = stroke(1, "#707070", 0.4)
+    theme.gloss = imageStyle("shadow-button-middle.png", color(1, 1, 1, 0.27))
+    theme.cursor = parseHtml("#77D3FF", 0.33)
+    theme.itemSpacing = 0.001 * fs
