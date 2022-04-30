29c00558df23828d0be43147992af0a5c66a131f
Jaremy Creechley <creechley@gmail.com>
2022-04-30 06:04:48 -0700

    lixt box

---
+++ src/fidgets/listbox.nim
@@ -0,0 +1,92 @@
+
+import widgets
+import button
+
+proc listbox*(
+    items {.property: items.}: seq[string],
+    selected {.property: selected.}: var int,
+): DropdownState {.statefulFidget.} =
+  ## dropdown widget 
+  init:
+    size 8'em, 1.5'em
+    cornerRadius theme
+    stroke theme.outerStroke
+    imageOf theme.gloss
+
+  properties:
+    itemsVisible: int
+
+  render:
+    let
+      cb = current.box()
+      bw = cb.w
+      bh = cb.h
+      bih = bh * 1.0
+      tw = bw - 1.5'em
+
+    let
+      visItems = items.len()
+      itemCount = max(1, visItems).min(items.len())
+      bdh = min(bih * itemCount.float32, windowLogicalSize.y/2)
+
+    if itemCount <= 2:
+      self.itemsVisible = items.len()
+      refresh()
+
+    proc resetState() = 
+      self.itemsVisible = -1
+
+    let spad = 1.0'f32
+
+    group "dropDownScroller":
+      box 0, bh, bw, bdh
+
+      clipContent true
+      zlevel ZLevelRaised
+      cornerRadius theme.cornerRadius
+      stroke theme.innerStroke
+
+      group "menuoutline":
+        box 0, 0, bw, bdh
+        cornerRadius local
+        stroke theme.outerStroke
+
+      group "menu":
+        # box spad, 6*spad, bw, bdh-6*spad
+        # box 0, this.cornerRadius[0]/2, bw, bdh+2*this.cornerRadius[0]
+        box 0, 0, bw, bdh
+        layout lmVertical
+        counterAxisSizingMode csAuto
+        itemSpacing -1
+        scrollBars true
+        # clipContent true
+
+        onClickOutside:
+          resetState()
+
+        var itemsVisible = -1
+        for idx, buttonName in pairs(items):
+          # itemSpacing 0.1'em
+          group "menuBtn":
+            if current.screenBox.overlaps(scrollBox):
+              itemsVisible.inc()
+            box 0, 0, bw, bih
+            layoutAlign laCenter
+
+            let clicked = widget button:
+              text: buttonName
+              setup:
+                clearShadows()
+                let ic = local.image.color
+                imageColor Color(r: 0, g: 0, b: 0, a: 0.20 * ic.a)
+                boxOf parent
+                cornerRadius 0
+                stroke theme.innerStroke
+            if clicked:
+              resetState()
+              selected = idx
+
+        if self.itemsVisible >= 0:
+          self.itemsVisible = min(itemsVisible, self.itemsVisible)
+        else:
+          self.itemsVisible = itemsVisible
