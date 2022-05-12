f5d1906b25b58b62cad2e2894c74399f1f7be120
Jaremy Creechley <creechley@gmail.com>
2022-05-12 00:45:52 -0700

    fixup dropdown

--- tests/testDropdown.nim
+++ tests/testDropdown.nim
@@ -1,15 +1,16 @@
-import fidgets
-import fidgets/dropdown
+import fidgetty
+import fidgetty/dropdown
   
 let dropItems = @["Nim", "UI", "in", "100%", "Nim", "to", 
                   "OpenGL", "Immediate", "mode"]
 var dropIndexes = [-1, -1, -1]
 
+loadFont("IBM Plex Sans", "IBMPlexSans-Regular.ttf")
 var dstate = DropdownState()
 
 proc drawMain() =
   frame "main":
-    font "IBM Plex Sans", 16, 200, 0, hLeft, vBottom
+    font "IBM Plex Sans", 14, 200, 0, hCenter, vCenter
     box 1'em, 1'em, 100'pw - 1'em, 100'ph - 1'em
 
     vertical:
@@ -20,8 +21,8 @@ proc drawMain() =
         fill "#000d00"
         characters "Dropdown example: "
 
-      dropdown(dropItems, dropIndexes[0], dstate)
-      dropdown(dropItems, dropIndexes[1], nil)
+      dropdown(dropItems, dropIndexes[0], "Dropdown", dstate)
+      dropdown(dropItems, dropIndexes[1], "Dropdown", nil)
       text "desc":
         size 100'pw, 1'em
         fill "#000d00"
@@ -29,8 +30,10 @@ proc drawMain() =
       dropdown(dropItems, dropIndexes[2])
       widget dropdown:
         items: dropItems
-        dropSelected: dropIndexes[2]
-        setup: box 0, 0, 12'em, 2'em
+        selected: dropIndexes[2]
+        setup:
+          box 0, 0, 12'em, 2'em
       
 
-startFidget(drawMain, uiScale=2.0, w=600, h=300)
+startFidget(drawMain, theme = grayTheme, uiScale=2.0, w=600, h=300)
+  
