38e59f32cfa5be5f2844b9e51843a750cc00f9a8
Jaremy Creechley <creechley@gmail.com>
2022-09-17 22:07:17 -0700

    fix basic

--- tests/testBasicWidget.nim
+++ tests/testBasicWidget.nim
@@ -1,113 +1,53 @@
-
-import fidget_dev, math, random
-import std/strformat
-import asyncdispatch # This is what provides us with async and the dispatcher
-import times, strutils # This is to provide the timing output
+import std/strformat, std/hashes
 
 import fidgetty
-import fidgetty/widgets
-import fidgetty/themes
-import fidgetty/[button, progressbar]
 
 loadFont("IBM Plex Sans", "IBMPlexSans-Regular.ttf")
 
-proc exampleApp*(
-    myName : string,
-): ExampleApp {.appFidget.} =
-  ## defines a stateful app widget
-  ## 
-  ## `exampleApp` will be transformed to also take the basic
-  ## widget parameters:
-  ##  - self: ExampleApp
-  ##  - setup: proc()
-  ##  - post: proc()
-  ## 
-  ## These parameters support using the widget with the properties
-  ## syntax. See the `button` examples below. The property name is
-  ## either the argument name or can be set using the `property` pragma
-  ## like show with `myName` that will provide a property of `name`.
-  ## 
-  
+fidgetty CounterButton:
   properties:
-    ## this creates a new ref object type name using the
-    ## capitalized proc name which is `ExampleApp` in this example. 
-    ## This will be customizable in the future. 
+    label: string
+  state:
     count: int
-    value: float
 
-  render:
-    setTitle(fmt"Fidget Animated Progress Example - {myName}")
-    font "IBM Plex Sans", 16, 200, 0, hCenter, vCenter
-    fill "#F7F7F9"
+proc new*(_: typedesc[CounterButtonProps]): CounterButtonProps =
+  new result
+  fill palette.foreground
 
-    group "center":
-      box 50, 0, 100.Vw - 100, 100.Vh
-      orgBox 50, 0, 100.Vw, 100.Vw
-      fill "#DFDFE0"
-      strokeWeight 1
+proc render*(
+    props: CounterButtonProps,
+    self: CounterButtonState
+): Events =
+  clipContent true
+  cornerRadius theme.cornerRadius
+  onHover:
+    highlight palette
+  onClick:
+    self.count.inc()
+  text "counter button":
+    boxSizeOf parent
+    fill palette.text
+    characters  fmt"label ({self.count})"
+    textAutoResize tsHeight
 
-      self.value = (self.count.toFloat * 0.10) mod 1.0001
-      progressbar(self.value, fmt"Progress: {self.value:4.2}") do:
-        box 10.WPerc, 20, 80.WPerc, 2.Em
+type
+  AppState* = ref object
+    count1: int
 
-      Horizontal:
-        # creates an horizontal spacing box
-
-        box 90.WPerc - 16.Em, 100, 8.Em, 2.Em
-        itemSpacing 0.Em
-
-        # Click to make the bar increase
-        # basic syntax just calling a proc
-        if button(fmt"Clicked1: {self.count:4d}"):
-          self.count.inc()
-
-        # Alternate format using `Widget` macro that enables
-        # a YAML like syntax using property labels
-        # (see parameters on `button` widget proc)
-        Button:
-          label: fmt"Clicked2: {self.count:4d}"
-          onClick: self.count.inc()
-
-        # current limit on Widget macros is that all args
-        # must be called as properties, no mix and match
-        #
-        # i.e. this doesn't work (yet):
-        #     Widget button(fmt"Clicked2: {self.count:4d}"):
-        #       onClick: self.count.inc()
-
-      Vertical:
-        # creates a vertical spacing box
-
-        box 10.WPerc, 160, 8.Em, 2.Em
-        itemSpacing 1.Em
-
-        # Button:
-        #   # default alias of `Widget button`
-        #   # only created for non-stateful widgets
-        #   text: fmt"Clicked3: {self.count:4d}"
-        #   setup: size 8.Em, 2.Em
-        #   onClick: self.count.inc()
-
-        Button:
-          label: fmt"Clicked4: {self.count:4d}"
-          setup: size 8.Em, 2.Em
-          onClick: self.count.inc()
-
-var state = ExampleApp(count: 2, value: 0.33)
-
-const callform {.intdefine.} = 2
-
-proc drawMain() =
+proc exampleApp*() =
+  ## defines a stateful app widget
   frame "main":
-    # we call exampleApp with a pre-made state
-    # the `statefulWidget` always takes a `self` paramter
-    # that that widgets state reference 
-    # alternatively:
-    exampleApp("basic widgets", state)
+    font "IBM Plex Sans", 16, 200, 0, hCenter, vCenter
+    useState(AppState, self)
+
+    CounterButton:
+      centeredXY 8'em, 2'em
+      onClick:
+        echo "hi!"
 
 
 startFidget(
-  drawMain,
+  exampleApp,
   setup = 
     when defined(demoBulmaTheme): setup(bulmaTheme)
     else: setup(grayTheme),
