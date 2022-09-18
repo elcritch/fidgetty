c4ea9966924227f1778754ac1bf366daffa35cc5
Jaremy Creechley <creechley@gmail.com>
2022-09-17 23:01:43 -0700

    fixing temp converter

--- tests/testTempConverter.nim
+++ tests/testTempConverter.nim
@@ -23,59 +23,82 @@ func toC*(val: Fahrenheit): Celsius =
 func toC*(val: Celsius): Celsius =
   val
 
-template parseTemp(val, kind: untyped) =
-  if `val`.updated.isSome():
-    var res: float
-    if parseFloat(`val`.updated.get().strip(), res, 0) > 0:
-      self.temp = `kind`(res).toC()
+# template parseTemp(val, kind: untyped) =
+#   if `val`.updated.isSome():
+#     var res: float
+#     if parseFloat(`val`.updated.get().strip(), res, 0) > 0:
+#       self.temp = `kind`(res).toC()
 
-template LabeledTextInput(valName, conv, fstr: untyped) =
-    FieldRight(label = fstr.repr()):
-      with:
-        let valName {.inject.} =
-          TextInput:
-            value:
-              block:
-                let sval {.inject.} = conv(self.temp).float32
-                fmt"{sval:5.1f}".strip()
-            setup:
-              size 5'em, 2'em
-            ignorePostfix: true
-            pattern: re"[0-9\.]"
-
-proc exampleApp*(): ExampleApp {.appFidget.} =
-  ## defines a stateful app widget
-  properties:
+type
+  ExampleApp = ref object
     temp: Celsius
 
-  render:
-    setTitle(fmt"Fidget Animated Progress Example")
-    textStyle theme
-    fill palette.background
-    box 0, 0, 100'vw, 100'vh
-    setWindowBounds(vec2(400, 200), vec2(600, 400))
+import print
 
-    Centered:
-      rectangle:
-        box 0, 0, 400, 200
-        fill "#DEDEDE"
-        cornerRadius 0.5'em
+proc labeledTextInput[T](
+    self: ExampleApp,
+) =
+  let sval =
+    when T is Fahrenheit: toF(self.temp).float
+    else: self.temp.float
+  
+  TextInput:
+    value fmt"{sval:5.1f}".strip()
+    size 5'em, 2'em
+    pattern re"[0-9\.]"
+    ignorePostfix true
+  finally:
+    processEvents(ValueChange):
+      Strings(val):
+        var res: float
+        if parseFloat(val.strip(), res, 0) > 0:
+          print "update: ", T(res)
+          self.temp = T(res).toC()
+          refresh()
 
-        Centered:
-          Horizontal:
-            # deg C
-            LabeledTextInput(cVal, toC, Celsius)
-            cVal.parseTemp(Celsius)
+proc exampleApp*() =
+  ## defines a stateful app widget
+  useState(ExampleApp, self)
 
-            basicLabel(" = ")
+  setTitle(fmt"Fidget Animated Progress Example")
+  textStyle theme
+  fill palette.background
+  box 0, 0, 100'vw, 100'vh
+  setWindowBounds(vec2(400, 200), vec2(600, 400))
 
-            # deg F
-            LabeledTextInput(fVal, toF, Fahrenheit)
-            fVal.parseTemp(Fahrenheit)
+  Centered:
+    rectangle:
+      box 0, 0, 400, 200
+      fill "#DEDEDE"
+      cornerRadius 0.5'em
+
+      Centered:
+        Horizontal:
+          # deg C
+          text:
+            fill palette.text
+            let lbl1 = "Celsius: "
+            characters lbl1
+            size lbl1.len().float.Em/2.0, 2'em
+          labeledTextInput[Celsius](self)
+          # cVal.parseTemp(Celsius)
+          text:
+            fill palette.text
+            characters " = "
+            size 3'em, 2'em
+
+          # deg F
+          text:
+            fill palette.text
+            let lbl2 = "Fahrenheit: "
+            characters lbl2
+            size lbl2.len().float.Em/2.0, 2'em
+          labeledTextInput[Fahrenheit](self)
+          # fVal.parseTemp(Fahrenheit)
 
 
 startFidget(
-  wrapApp(exampleApp, ExampleApp),
+  exampleApp,
   setup = 
     when defined(demoBulmaTheme): setup(bulmaTheme)
     else: setup(grayTheme),
