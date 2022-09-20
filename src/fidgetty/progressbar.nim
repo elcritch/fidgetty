bdf041a3b6a238403bde6dda47bad5fd52429122
Jaremy Creechley <creechley@gmail.com>
2022-09-20 03:58:23 -0700

    updates

--- src/fidgetty/progressbar.nim
+++ src/fidgetty/progressbar.nim
@@ -29,22 +29,32 @@ proc render*(
     sbb = 2.0*sb
     wcalc = bw * props.value.clamp(0, 1.0).UICoord - sbb + 0.001
     barW = wcalc.clamp(0.0, bw-sb)
+    cbarW = (100.0 * props.value.clamp(0, 1.0)).csPerc()
     barH = bh - sb
+    cbw = 100'pp
+    cbarH = 90'pp
+    cb = 1'em
 
   if props.label.len() > 0:
     text "text":
-      box 0, 0, bw, bh
+      box 0, 0, cbarW, cbarH
       fill palette.text
       characters props.label
 
   rectangle "barFgTexture":
-    box sb, sb, barW, barH-sb
+    # box 0, 0, barW, barH-sb
+    box cb, cb, cbarW, cbarH
+    # size barPP, 80'pp
+    # offset cb, cb
     cornerRadius 0.80 * theme.cornerRadius[0]
     clipContent true
     # strokeLine 1.0, "#707070", 0.87
 
   rectangle "barFgColor":
-    box sb, sb, barW, barH-sb
+    # box 0, 0, barW, barH-sb
+    box cb, cb, cbarW, cbarH
+    # size barPP, 80'pp
+    # offset cb, cb
     fill palette.accent
     cornerRadius 0.80 * theme.cornerRadius[0]
     imageOf theme.gloss, 0.67
@@ -53,7 +63,9 @@ proc render*(
 
   rectangle "bar":
     # Draw the bar itself.
-    box 0, 0, bw, bh
+    # box 0, 0, bw, bh
+    box 0, 0, 100'pp, 100'pp
+    # size 100'pp, 100'pp
     stroke theme.outerStroke
     fill palette.foreground
     cornerRadius 1.0 * theme.cornerRadius[0]
