dcd07ae582f564d482e1520013d02294aaa0272d
Jaremy Creechley <creechley@gmail.com>
2022-09-23 00:51:38 -0700

    tweaks

--- src/fidgetty/slider.nim
+++ src/fidgetty/slider.nim
@@ -37,10 +37,6 @@ proc render*(
     characters props.label
   rectangle "bar holder":
     gridArea 2 // 3, 2 // 3
-    fill palette.accent
-    cornerRadius theme
-    clipContent true
-    imageOf theme.gloss, 0.77
 
     let popBtnWidth = height()
     let popTrackWidth = width() - popBtnWidth
@@ -50,15 +46,23 @@ proc render*(
       if props.value != self.value:
         dispatchEvent Float(self.value)
 
+    let pipFrac = UICoord(props.value).clamp(0'ui, 1'ui)
+    let pipPos = popTrackWidth*pipFrac
+
     rectangle "pop button":
-      let pipFrac = UICoord(props.value).clamp(0'ui, 1'ui)
-      let pipPos = popTrackWidth*pipFrac
       box pipPos, 0, parent.box.h, parent.box.h
       fill palette.cursor
       cornerRadius theme
       stroke theme.outerStroke
       clipContent true
       imageOf theme.gloss
+
+    rectangle "bar holder":
+      box 0, 0, pipPos + parent.box.h/2, parent.box.h
+      fill palette.accent
+      cornerRadius theme
+      clipContent true
+      imageOf theme.gloss, 0.77
 
   rectangle "bar bg":
     gridArea 1 // 4, 1 // 4
